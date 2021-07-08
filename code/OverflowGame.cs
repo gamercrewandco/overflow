258a561abc23bc1dd92223e1c1de8b2534b82f63
taek-dev <76453783+taek-dev@users.noreply.github.com>
2021-07-07 20:00:15 -0400

    basic stuf

---
+++ code/OverflowGame.cs
@@ -0,0 +1,48 @@
+﻿using Sandbox;
+using Sandbox.UI.Construct;
+using Sandbox.UI;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace overflow
+{
+	[Library( "overflow" )]
+	public partial class OverflowGame : Game
+    {
+		public static new OverflowGame Current => Game.Current as OverflowGame;
+
+		public OverflowGame()
+		{
+			if ( IsServer )
+			{
+				Log.Info( "My Gamemode Has Created Serverside!" );
+			}
+
+			if ( IsClient )
+			{
+				Log.Info( "My Gamemode Has Created Clientside!" );
+			}
+		}
+
+		public override void Simulate( Client cl )
+		{
+			base.Simulate( cl );
+
+
+		}
+
+		// an epic gamer just joined the server, better give them an epic gamer pawn to play with
+		public override void ClientJoined( Client client )
+		{
+			base.ClientJoined( client );
+
+			var player = new OverflowPlayer();
+			client.Pawn = player;
+
+			player.Respawn();
+		}
+	}
+
+
+}
---
+++ code/OverflowPlayer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sandbox;
+
+namespace overflow
+{
+	partial class OverflowPlayer : Player
+	{
+		public override void Respawn()
+		{
+			SetModel( "models/citizen/citizen.vmdl" );
+
+			// set controller, animator, and camera (pretty cool)
+			Controller = new WalkController();
+			Animator = new StandardPlayerAnimator();
+			Camera = new ThirdPersonCamera();
+
+			EnableAllCollisions = true;
+			EnableDrawing = true;
+			EnableHideInFirstPerson = true;
+			EnableShadowInFirstPerson = true;
+
+			base.Respawn();
+		}
+
+		public override void OnKilled()
+		{
+			Controller = new NoclipController();
+			Camera = new FirstPersonCamera();
+			Velocity = Vector3.Zero;
+
+			EnableAllCollisions = false;
+			EnableDrawing = false;
+		}
+	}
+}
