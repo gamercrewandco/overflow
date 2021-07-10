0f4abc75c169a3f3507f9ab8b8833cf9ca7d3870
Plaonder Joander <biz@jayden.phindog.family>
2021-07-10 15:49:31 -0700

    The players are finally not naked

--- code/OverflowPlayer.cs
+++ code/OverflowPlayer.cs
@@ -18,6 +18,14 @@ namespace overflow
 		public Color killedColor;
 		public Color escapedColor;
 
+		ModelEntity pants;
+		ModelEntity jacket;
+		ModelEntity shoes;
+		ModelEntity hat;
+
+		bool dressed = false;
+
+
 		public override void Respawn()
 		{
 			SetModel( "models/citizen/citizen.vmdl" );
@@ -26,7 +34,7 @@ namespace overflow
 			Controller = new WalkController();
 			Animator = new StandardPlayerAnimator();
 			Camera = new ThirdPersonCamera();
-
+			
 			EnableAllCollisions = true;
 			EnableDrawing = true;
 			EnableHideInFirstPerson = true;
@@ -34,6 +42,8 @@ namespace overflow
 
 			killedColor = Color.Red;
 			escapedColor = Color.Green;
+
+			Dress();
 
 			base.Respawn();
 		}
@@ -97,6 +107,111 @@ namespace overflow
 		public virtual void OnKilledMessage( Color color, string text )
 		{
 			WinLoseFeed.Current?.AddEntry( color, text );
+		}
+		
+		public void Dress()
+		{
+			Log.Info("I am dressing up.");
+			
+			if ( dressed ) return;
+			dressed = true;
+
+			if ( true )
+			{
+				Log.Info( "Trying to put on my pants." );
+				var model = Rand.FromArray( new[]
+				{
+				"models/citizen_clothes/trousers/trousers.jeans.vmdl",
+				"models/citizen_clothes/trousers/trousers.lab.vmdl",
+				"models/citizen_clothes/trousers/trousers.police.vmdl",
+				"models/citizen_clothes/trousers/trousers.smart.vmdl",
+				"models/citizen_clothes/trousers/trousers.smarttan.vmdl",
+				"models/citizen/clothes/trousers_tracksuit.vmdl",
+				"models/citizen_clothes/trousers/trousers_tracksuitblue.vmdl",
+				"models/citizen_clothes/trousers/trousers_tracksuit.vmdl",
+				"models/citizen_clothes/shoes/shorts.cargo.vmdl",
+			} );
+
+				pants = new ModelEntity();
+				pants.SetModel( model );
+				pants.SetParent( this, true );
+				pants.EnableShadowInFirstPerson = true;
+				pants.EnableHideInFirstPerson = true;
+
+				pants.SetBodyGroup( "Legs", 1 );
+			}
+
+			if ( true )
+			{
+				var model = Rand.FromArray( new[]
+				{
+				"models/citizen_clothes/jacket/labcoat.vmdl",
+				"models/citizen_clothes/jacket/jacket.red.vmdl",
+				"models/citizen_clothes/jacket/jacket.tuxedo.vmdl",
+				"models/citizen_clothes/jacket/jacket_heavy.vmdl",
+			} );
+
+				jacket = new ModelEntity();
+				jacket.SetModel( model );
+				jacket.SetParent( this, true );
+				jacket.EnableShadowInFirstPerson = true;
+				jacket.EnableHideInFirstPerson = true;
+
+				var propInfo = jacket.GetModel().GetPropData();
+				if ( propInfo.ParentBodyGroupName != null )
+				{
+					jacket.SetBodyGroup( propInfo.ParentBodyGroupName, propInfo.ParentBodyGroupValue );
+				}
+				else
+				{
+					jacket.SetBodyGroup( "Chest", 0 );
+				}
+			}
+
+			if ( true )
+			{
+				var model = Rand.FromArray( new[]
+				{
+				"models/citizen_clothes/shoes/trainers.vmdl",
+				"models/citizen_clothes/shoes/shoes.workboots.vmdl"
+			} );
+
+				shoes = new ModelEntity();
+				shoes.SetModel( model );
+				shoes.SetParent( this, true );
+				shoes.EnableShadowInFirstPerson = true;
+				shoes.EnableHideInFirstPerson = true;
+
+				shoes.SetBodyGroup( "Feet", 1 );
+			}
+
+			if ( true )
+			{
+				var model = Rand.FromArray( new[]
+				{
+				"models/citizen_clothes/hat/hat_hardhat.vmdl",
+				"models/citizen_clothes/hat/hat_woolly.vmdl",
+				"models/citizen_clothes/hat/hat_securityhelmet.vmdl",
+				"models/citizen_clothes/hair/hair_malestyle02.vmdl",
+				"models/citizen_clothes/hair/hair_femalebun.black.vmdl",
+				"models/citizen_clothes/hat/hat_beret.red.vmdl",
+				"models/citizen_clothes/hat/hat.tophat.vmdl",
+				"models/citizen_clothes/hat/hat_beret.black.vmdl",
+				"models/citizen_clothes/hat/hat_cap.vmdl",
+				"models/citizen_clothes/hat/hat_leathercap.vmdl",
+				"models/citizen_clothes/hat/hat_leathercapnobadge.vmdl",
+				"models/citizen_clothes/hat/hat_securityhelmetnostrap.vmdl",
+				"models/citizen_clothes/hat/hat_service.vmdl",
+				"models/citizen_clothes/hat/hat_uniform.police.vmdl",
+				"models/citizen_clothes/hat/hat_woollybobble.vmdl",
+			} );
+
+				hat = new ModelEntity();
+				hat.SetModel( model );
+				//hat.SetParent( this, true );
+				hat.EnableShadowInFirstPerson = true;
+				hat.EnableHideInFirstPerson = true;
+			}
 		}
 	}
 }
